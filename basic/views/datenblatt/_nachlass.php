af456695857ddc82ba631dba6c95bdefe16e8bc8
Erdal Mersinlioglu <erdal.mersinlioglu@gmail.com>
2016-01-25 23:38:11 +0100

    accordion

--- basic/views/datenblatt/_nachlass.php
+++ basic/views/datenblatt/_nachlass.php
@@ -6,84 +6,87 @@ use yii\helpers\Html;
 /* @var $modelNachlass app\models\Nachlass */
 /* @var $form yii\bootstrap\ActiveForm */
 ?>
-
-
-<div class="row">
-    <div class="col-sm-8">
-        <h3>Minderungen/Nachlass:</h3>
-    </div>
-    <div class="col-sm-3">
-        <?php 
-        $total = 0;
-        foreach($modelDatenblatt->nachlasses as $item) {
-            $total += $item->betrag;
-        } ?>
-        <h3>Summe: <?= Yii::$app->formatter->asCurrency($total) ?></h3>
-    </div>
-</div>
- 
-<div class="row">
-    <div class="col-sm-3">
-        <!--?= Html::submitButton('<span class="fa fa-plus"> Sonderwunsch hinzufügen</span>', ['class' => 'btn btn-success', 'name' => 'addSonderwunsch']) ?-->
-        
-    </div>
-</div>
-
-<table class="table table-bordered">
-    <tr>
-        <th>Schreiben vom</th>
-        <th>Betrag</th>
-        <th>Bemerkung</th>
-        <th><?= Html::a('<span class="fa fa-plus"> </span>',
-            Yii::$app->urlManager->createUrl(["datenblatt/addnachlass", 'datenblattId' => $modelDatenblatt->id]), 
-            ['class' => 'add-zahlung btn btn-success btn-xl']) ?></th>
-    </tr>
-<?php 
-
-$rechnungstellungBetrag = 0;
-
-foreach($modelDatenblatt->nachlasses as $key => $modelNachlass): ?>
-<tr class="sonderwunsch">
-    <td>
-        <div class="hide">
-            <?= $form->field($modelNachlass, "[$key]id")->textInput() ?>
+<div class="box-group" id="accordion">
+    <!-- we are adding the .panel class so bootstrap.js collapse plugin detects it -->
+    <div class="panel box box-primary">
+        <div class="box-header with-border">
+            <h4 class="box-title">
+                <a data-toggle="collapse" data-parent="#accordion" href="#collapse-nachlass" aria-expanded="true" class="">
+                    Minderungen/Nachlass:
+                    
+                    <?php 
+                    $total = 0;
+                    foreach($modelDatenblatt->nachlasses as $item) {
+                        $total += $item->betrag;
+                    } ?>
+                    (Summe: <?= Yii::$app->formatter->asCurrency($total) ?>)
+                </a>
+            </h4>
         </div>
-        <?php
-            $datum = DateTime::createFromFormat('Y-m-d H:i:s', $modelNachlass->schreiben_vom);
-            if ($datum) {
-                $datum = $datum->format('d.m.Y');
-            } else {
-                $datum = '';
-            }
-            //echo '<label>Übergang BNL:</label>';
-            echo DateTimePicker::widget([
-                'name' => "Nachlass[$key][schreiben_vom]",
-                'options' => ['placeholder' => 'Datum auswählen'],
-                'convertFormat' => true,
-                'value' => $datum,
-                'pluginOptions' => [
-                    'minView' => 'month',
-                    'maxView' => 'month',
-                    'viewSelect' => 'month',
-                    'format' => 'dd.mm.yyyy',
-                    'autoclose' => true,
-                    'todayHighlight' => true
-                ]
-            ]);
-        ?>
-    </td>
-    <td>
-        <?= $form->field($modelNachlass, "[$key]betrag")->textInput([]) ?>
-    </td>
-    <td>
-        <?= $form->field($modelNachlass, "[$key]bemerkung")->textInput([]) ?>
-    </td>
-    <td>
-        <?= Html::a('<span class="fa fa-minus"></span>', 
-            Yii::$app->urlManager->createUrl(["datenblatt/deletenachlass", 'datenblattId' => $modelDatenblatt->id , 'nachlassId' => $modelNachlass->id]), 
-            ['class' => 'add-zahlung btn btn-danger btn-xl']) ?>
-    </td>
-</tr>    
-<?php endforeach;  ?>
+        <div id="collapse-nachlass" class="panel-collapse collapse" aria-expanded="false">
+            <div class="box-body">
+                
+                <table class="table table-bordered">
+                    <tr>
+                        <th>Schreiben vom</th>
+                        <th>Betrag</th>
+                        <th>Bemerkung</th>
+                        <th><?= Html::a('<span class="fa fa-plus"> </span>',
+                            Yii::$app->urlManager->createUrl(["datenblatt/addnachlass", 'datenblattId' => $modelDatenblatt->id]), 
+                            ['class' => 'add-zahlung btn btn-success btn-xl']) ?></th>
+                    </tr>
+                <?php 
 
-</table>
+                $rechnungstellungBetrag = 0;
+
+                foreach($modelDatenblatt->nachlasses as $key => $modelNachlass): ?>
+                <tr class="sonderwunsch">
+                    <td>
+                        <div class="hide">
+                            <?= $form->field($modelNachlass, "[$key]id")->textInput() ?>
+                        </div>
+                        <?php
+                            $datum = DateTime::createFromFormat('Y-m-d H:i:s', $modelNachlass->schreiben_vom);
+                            if ($datum) {
+                                $datum = $datum->format('d.m.Y');
+                            } else {
+                                $datum = '';
+                            }
+                            //echo '<label>Übergang BNL:</label>';
+                            echo DateTimePicker::widget([
+                                'name' => "Nachlass[$key][schreiben_vom]",
+                                'options' => ['placeholder' => 'Datum auswählen'],
+                                'convertFormat' => true,
+                                'value' => $datum,
+                                'pluginOptions' => [
+                                    'minView' => 'month',
+                                    'maxView' => 'month',
+                                    'viewSelect' => 'month',
+                                    'format' => 'dd.mm.yyyy',
+                                    'autoclose' => true,
+                                    'todayHighlight' => true
+                                ]
+                            ]);
+                        ?>
+                    </td>
+                    <td>
+                        <?= $form->field($modelNachlass, "[$key]betrag")->textInput([]) ?>
+                    </td>
+                    <td>
+                        <?= $form->field($modelNachlass, "[$key]bemerkung")->textInput([]) ?>
+                    </td>
+                    <td>
+                        <?= Html::a('<span class="fa fa-minus"></span>', 
+                            Yii::$app->urlManager->createUrl(["datenblatt/deletenachlass", 'datenblattId' => $modelDatenblatt->id , 'nachlassId' => $modelNachlass->id]), 
+                            ['class' => 'add-zahlung btn btn-danger btn-xl']) ?>
+                    </td>
+                </tr>    
+                <?php endforeach;  ?>
+
+                </table>
+                
+            </div>
+        </div>
+    </div>
+</div>
+
--- basic/views/datenblatt/_zahlung.php
+++ basic/views/datenblatt/_zahlung.php
@@ -7,77 +7,93 @@ use yii\helpers\Html;
 /* @var $form yii\bootstrap\ActiveForm */
 ?>
 
-<div class="row">
-    <div class="col-sm-8">
-        <h3>Zahlungen:</h3>
-    </div>
-    <div class="col-sm-3">
-        <?php 
-        $total = 0;
-        foreach($modelDatenblatt->zahlungs as $key => $modelZahlung) {
-            $total += $modelZahlung->betrag;
-        } ?>
-        <h3>Summe: <?= Yii::$app->formatter->asCurrency($total) ?></h3>
+<div class="box-group" id="accordion">
+    <!-- we are adding the .panel class so bootstrap.js collapse plugin detects it -->
+    <div class="panel box box-primary">
+        <div class="box-header with-border">
+            <h4 class="box-title">
+                <a data-toggle="collapse" data-parent="#accordion" href="#collapse-zahlung" aria-expanded="true" class="">
+                    Zahlungen:
+                    
+                    <?php 
+                    $total = 0;
+                    foreach($modelDatenblatt->zahlungs as $key => $modelZahlung) {
+                        $total += $modelZahlung->betrag;
+                    } ?>
+                    (Summe: <?= Yii::$app->formatter->asCurrency($total) ?>)
+                </a>
+            </h4>
+        </div>
+        <div id="collapse-zahlung" class="panel-collapse collapse" aria-expanded="false">
+            <div class="box-body">
+
+                <table class="table table-bordered">
+                    <tr>
+                        <th>Datum</th>
+                        <th>Betrag</th>
+                        <th>Bemerkung</th>
+                        <th>
+                            <?= Html::a('<span class="fa fa-plus"> </span>',
+                            Yii::$app->urlManager->createUrl(["datenblatt/addzahlung", 'datenblattId' => $modelDatenblatt->id]), 
+                            ['class' => 'add-zahlung btn btn-success btn-xl']) ?>
+                        </th>
+                    </tr>
+                <?php 
+
+                $rechnungstellungBetrag = 0;
+
+                foreach($modelDatenblatt->zahlungs as $key => $modelZahlung): ?>
+                <tr class="sonderwunsch">
+                    <td>
+                        <div class="hide">
+                            <?= $form->field($modelZahlung, "[$key]id")->textInput() ?>
+                        </div>
+                        <?php
+                            $datum = DateTime::createFromFormat('Y-m-d H:i:s', $modelZahlung->datum);
+                            if ($datum) {
+                                $datum = $datum->format('d.m.Y');
+                            } else {
+                                $datum = '';
+                            }
+                            //echo '<label>Übergang BNL:</label>';
+                            echo DateTimePicker::widget([
+                                'name' => "Zahlung[$key][datum]",
+                                'options' => ['placeholder' => 'Datum auswählen'],
+                                'convertFormat' => true,
+                                'value' => $datum,
+                                'pluginOptions' => [
+                                    'minView' => 'month',
+                                    'maxView' => 'month',
+                                    'viewSelect' => 'month',
+                                    'format' => 'dd.mm.yyyy',
+                                    'autoclose' => true,
+                                    'todayHighlight' => true
+                                ]
+                            ]);
+                        ?>
+                    </td>
+                    <td>
+                        <?= $form->field($modelZahlung, "[$key]betrag")->textInput([]) ?>
+                    </td>
+                    <td>
+                        <?= $form->field($modelZahlung, "[$key]bemerkung")->textInput([]) ?>
+                    </td>
+                    <td>
+                        <?= Html::a('<span class="fa fa-minus"></span>', 
+                            Yii::$app->urlManager->createUrl(["datenblatt/deletezahlung", 'datenblattId' => $modelDatenblatt->id , 'zahlungId' => $modelZahlung->id]), 
+                            ['class' => 'add-zahlung btn btn-danger btn-xl']) ?>
+                    </td>
+                </tr>    
+                <?php endforeach;  ?>
+
+                </table>
+                
+                
+                
+                
+            </div>
+        </div>
     </div>
 </div>
 
-<table class="table table-bordered">
-    <tr>
-        <th>Datum</th>
-        <th>Betrag</th>
-        <th>Bemerkung</th>
-        <th>
-            <?= Html::a('<span class="fa fa-plus"> </span>',
-            Yii::$app->urlManager->createUrl(["datenblatt/addzahlung", 'datenblattId' => $modelDatenblatt->id]), 
-            ['class' => 'add-zahlung btn btn-success btn-xl']) ?>
-        </th>
-    </tr>
-<?php 
 
-$rechnungstellungBetrag = 0;
-
-foreach($modelDatenblatt->zahlungs as $key => $modelZahlung): ?>
-<tr class="sonderwunsch">
-    <td>
-        <div class="hide">
-            <?= $form->field($modelZahlung, "[$key]id")->textInput() ?>
-        </div>
-        <?php
-            $datum = DateTime::createFromFormat('Y-m-d H:i:s', $modelZahlung->datum);
-            if ($datum) {
-                $datum = $datum->format('d.m.Y');
-            } else {
-                $datum = '';
-            }
-            //echo '<label>Übergang BNL:</label>';
-            echo DateTimePicker::widget([
-                'name' => "Zahlung[$key][datum]",
-                'options' => ['placeholder' => 'Datum auswählen'],
-                'convertFormat' => true,
-                'value' => $datum,
-                'pluginOptions' => [
-                    'minView' => 'month',
-                    'maxView' => 'month',
-                    'viewSelect' => 'month',
-                    'format' => 'dd.mm.yyyy',
-                    'autoclose' => true,
-                    'todayHighlight' => true
-                ]
-            ]);
-        ?>
-    </td>
-    <td>
-        <?= $form->field($modelZahlung, "[$key]betrag")->textInput([]) ?>
-    </td>
-    <td>
-        <?= $form->field($modelZahlung, "[$key]bemerkung")->textInput([]) ?>
-    </td>
-    <td>
-        <?= Html::a('<span class="fa fa-minus"></span>', 
-            Yii::$app->urlManager->createUrl(["datenblatt/deletezahlung", 'datenblattId' => $modelDatenblatt->id , 'zahlungId' => $modelZahlung->id]), 
-            ['class' => 'add-zahlung btn btn-danger btn-xl']) ?>
-    </td>
-</tr>    
-<?php endforeach;  ?>
-
-</table>
