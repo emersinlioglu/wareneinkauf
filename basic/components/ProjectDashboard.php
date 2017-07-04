1f0c3b911ebc168e2dac85e30369418721caca95
Erdal Mersinlioglu <erdal.mersinlioglu@gmail.com>
2017-07-04 22:51:13 +0200

    Dashboard ist nur für superadmin sichtbar

--- basic/components/ProjectDashboard.php
+++ basic/components/ProjectDashboard.php
@@ -54,28 +54,9 @@ class ProjectDashboard extends Widget
 
         }
 
-//        echo "<pre>";
-//        print_r($verkaufsentwicklungData);
-//        print_r($verkaufsentwicklungDataProProjekt);
-
-
-//        $einheitstypModel = new Einheitstyp();
-//        $projektModel     = new Projekt();
-
-//        foreach ($projektModel->getWohnflaeschenDataFuerAlleProjekte() as $key => $row) {
-//            $verkaufsentwicklungData[] = [
-//                'name'      => $row['name'],
-//                'y'         => (float) $row['summeWohnflaeche'],
-//            ];
-//
-//            $projektData = $einheitstypModel->getProjektVerkaufsentwicklungData($row['projektId']);
-//            foreach ($projektData as $pKey => $pData) {
-//                $verkaufsentwicklungDataProProjekt[] = [
-//                    'name'      => $pData['projektName'],
-//                    'y'         => (float) $pData['summeWohnflaeche'],
-//                ];
-//            }
-//        }
+        if (!\Yii::$app->user->isSuperadmin) {
+            return '';
+        }
 
         return $this->render('projectDashboard', array(
             "projectDashboardData" => $projectDashboardData,
