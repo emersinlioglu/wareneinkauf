7b1a6042b8f63f6d6ddaf6d767257177676ed1dd
Erdal Mersinlioglu <erdal.mersinlioglu@gmail.com>
2017-08-20 23:38:07 +0200

    vorlage modalbox text

--- basic/views/vorlage/index.php
+++ basic/views/vorlage/index.php
@@ -1,8 +1,8 @@
 <?php
 
 use yii\helpers\Html;
-//use yii\grid\GridView;
-use kartik\grid\GridView;
+use yii\grid\GridView;
+//use kartik\grid\GridView;
 
 /* @var $this yii\web\View */
 /* @var $searchModel app\models\VorlageSearch */
@@ -14,6 +14,12 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="mail-vorlage-index">
 
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <style>
+        #myTextModal .modal-dialog {
+            width: 900px;
+        }
+    </style>
 
     <p>
         <?= Html::a('Vorlage erstellen', ['create'], ['class' => 'btn btn-success']) ?>
@@ -33,6 +39,35 @@ $this->params['breadcrumbs'][] = $this->title;
             [
                 'attribute' => 'text',
                 'format' => 'raw',
+//                '.\yii\helpers\StringHelper::truncate($model->text, 100).'
+                'value' => function ($model) {
+                    return '
+                        <button type="button" class="btn btn-info" data-toggle="modal" data-target="#myTextModal">
+                            Text
+                        </button>
+                        
+                        <!-- Modal -->
+                        <div id="myTextModal" class="modal large fade" role="dialog">
+                          <div class="modal-dialog" width="900">
+                        
+                            <!-- Modal content-->
+                            <div class="modal-content">
+                              <div class="modal-header">
+                                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                <h4 class="modal-title">Modal Header</h4>
+                              </div>
+                              <div class="modal-body">
+                                '.$model->text.'
+                              </div>
+                              <div class="modal-footer">
+                                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                              </div>
+                            </div>
+                        
+                          </div>
+                        </div>
+                    ';
+                }
 //                'detail' => 'adfasdfasdf'
             ],
 
