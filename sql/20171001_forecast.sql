2ba034bee73692dd0b7180248952ee012a3f2958
Erdal Mersinlioglu <erdal.mersinlioglu@gmail.com>
2017-10-22 19:37:13 +0200

    SQL fix

--- sql/20171001_forecast.sql
+++ sql/20171001_forecast.sql
@@ -1,6 +1,6 @@
 ALTER TABLE `teileigentumseinheit`
   ADD COLUMN `forecast_preis` DECIMAL(11,2) NULL DEFAULT '0' AFTER `kp_einheit`,
-  ADD COLUMN `verkaufspreis` DECIMAL(11,2) NULL DEFAULT '0' AFTER `forecast_price`,
+  ADD COLUMN `verkaufspreis` DECIMAL(11,2) NULL DEFAULT '0' AFTER `forecast_preis`,
   ADD COLUMN `verkaufspreis_begruendung` TEXT NULL AFTER `verkaufspreis`;
 
 ALTER TABLE `teileigentumseinheit`
