3b7287ca477428e825cbfc1351b5b5db61c96f02
Erdal Mersinlioglu <erdal.mersinlioglu@gmail.com>
2018-05-14 23:13:29 +0200

    SQL  update te-status von haus-status

--- sql/20180514_te_status.sql
+++ sql/20180514_te_status.sql
@@ -3,3 +3,7 @@ ALTER TABLE `teileigentumseinheit`
 
 ALTER TABLE `teileigentumseinheit`
     ADD COLUMN `rechnung_vertrieb` TINYINT(1) NULL DEFAULT '0' AFTER `status`;
+
+update teileigentumseinheit te
+    left join haus h on h.id = te.haus_id
+set te.`status` = h.status;
